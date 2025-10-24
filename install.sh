84ffb24443a72fd9392f9c4745397e2d0fc04fb5
Ricardo Sanchez <ricardo.stagle@gmail.com>
2025-10-24 17:53:42 -0600

    Updated and testing github actions

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#ddev stop --unlist *
 ddev start
 ddev import-db --file=database_backup.sql.gz
 ddev composer install
+#ddev composer update --lock
 ddev drush cr
 ddev drush updb -y
 ddev drush cim -y
